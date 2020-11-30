84920c003fd47ffcd9bb9ffaa8f26736014e223a
Fernando <fernandoghw@hotmail.com>
2020-11-30 19:08:31 -0300

    Aplicação funcionando de forma simples, ainda precisa popular os .JSON

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const fortune = require('./fortune.json')
+const subject = require('./subject.json')
+const express = require('express')
+const app = express()
+
+
+const randomArrayPosition = (objectArray) => {
+const index = Math.floor(Math.random()* (objectArray.length))
+return objectArray[index]
+};
+
+const randomFortuneVerbFiltered = (objectSubject) =>{
+
+const filteredFortune = fortune.filter((position) => (objectSubject.verb === position.verb || true === position.verb))
+ console.log(objectSubject.verb, Object.values(filteredFortune))
+return randomArrayPosition(filteredFortune)
+
+}
+
+
+
+function generateFortune(){
+
+const objectSubject = randomArrayPosition(subject);
+const objectFortune = randomFortuneVerbFiltered(objectSubject);
+
+return `${objectSubject.subject} ${objectFortune.fortune}`
+
+}
+
+console.log(generateFortune())
